fcc892f1e0d4b54cc646320898ca0f5b0e30c1fb
Matthew Sheets <matt.sheets@mac.com>
2018-07-19 09:43:59 -0500

    adding graphQL listener and express bug

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require ('express');
+const expressGraphQL = require ('express-graphql');
 
 const app = express();
 
-app.listem (4000, () => {
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}));
+
+app.listen (4000, () => {
     console.log('Listening on 4000');
 });
