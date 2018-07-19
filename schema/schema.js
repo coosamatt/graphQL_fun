e1c9f2486b5e4e61f1878324ce4a55c9df375744
Matthew Sheets <matt.sheets@mac.com>
2018-07-19 10:13:48 -0500

    adding const users

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,16 @@
 const graphql = requre('graphql');
+const _ = requre('lodash');
+
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt
 } = graphql;
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20},
+    { id: '47', firstName: 'Samantha', age: 21}
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
